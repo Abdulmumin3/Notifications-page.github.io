d4b8946e82cd48c2754a2023fd189cb22ad56feb
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-04-17 06:21:50 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "v-light-grayish-blue": "hsl(210, 60%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+        "light-grayish-blue-1": "hsl(211, 68%, 94%)",
+        "light-grayish-blue-2": "hsl(205, 33%, 90%)",
+        "grayish-blue": "hsl(219, 14%, 63%)",
+        "dark-grayish-blue": "hsl(219, 12%, 42%)",
+        "v-dark-blue": "hsl(224, 21%, 14%)",
+        "attrib-color": 'hsl(228, 45%, 44%)',
+      },
+    },
+  },
+  plugins: [],
+};
